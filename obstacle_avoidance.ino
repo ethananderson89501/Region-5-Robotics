3b39b9075b9b7b98eab4f9ba99cf4ce59dd3da1b
ethananderson89501 <ethananderson89501@gmail.com>
2024-12-08 21:33:10 -0600

    Updated 12/8/24
    Better logic for hugging the wall. Added safety precautions for bad sensor readings. Changed the structure of some functions for better memory optimization

--- obstacle_avoidance.ino
+++ obstacle_avoidance.ino
@@ -3,7 +3,6 @@
 #include <Wire.h>
 
 #define SERVO_PIN 9
-//#define LPT 2
 
 #define ECHO 8
 #define TRIG 7
@@ -15,54 +14,71 @@
 #define IN4 11 
 #define ENB 12
 
-int divisions = 6;
+#define stopped 0
+#define forward 1
+#define left 2
+#define right 3
+#define reverse 4
+
+const int divisions = 8;
 
 Servo head;
 
 int duty_cycleA;
 int duty_cycleB;
 
-void forward(){
+int state;
+
+void goForward(){
+  state = forward;
   duty_cycleA = 128;
   duty_cycleB = 128;
   analogWrite(IN1, 0);
   analogWrite(IN2, duty_cycleA);
   analogWrite(IN3, duty_cycleB);
   analogWrite(IN4, 0);
+  Serial.println("Going forward");
 }
 
 void stop(){
+  state = stop;
   analogWrite(IN1, 0);
   analogWrite(IN2, 0);
   analogWrite(IN3, 0);
   analogWrite(IN4, 0);
 }
 
-void reverse(){
+void goReverse(){
+  state = reverse;
   duty_cycleA = 128;
   duty_cycleB = 128;
   analogWrite(IN1, duty_cycleA);
   analogWrite(IN2, 0);
   analogWrite(IN3, 0);
   analogWrite(IN4, duty_cycleB);
+  Serial.println("Reversing");
 }
 
 void turnLeft(){
+  state = left;
   duty_cycleA = 192;
   duty_cycleB = 128;
   analogWrite(IN1, 0);
   analogWrite(IN2, duty_cycleA);
   analogWrite(IN3, 0);
   analogWrite(IN4, duty_cycleB);
+  Serial.println("Turning left");
 }
 
 void turnRight(){
+  state = right;
   duty_cycleA = 128;
   duty_cycleB = 192;
   analogWrite(IN1, duty_cycleA);
   analogWrite(IN2, 0);
   analogWrite(IN3, duty_cycleB);
   analogWrite(IN4, 0);
+  Serial.println("Turning right");
 }
 
 double watch(){
@@ -72,27 +88,38 @@ double watch(){
   delayMicroseconds(10); // give or take 7 feet
   digitalWrite(TRIG,LOW);
   long echo_time=pulseIn(ECHO,HIGH);
-  Serial.print("Echo time: ");
-  Serial.println(echo_time);
+  if (echo_time <= 0) {
+    return -1.0; // Return a special value to indicate no echo detected
+  }
+  //Serial.print("Echo time: ");
+  //Serial.println(echo_time);
   double echo_distance=echo_time / 148.0; //how far away is the object in inches
   return echo_distance;
 }
 
-double * scan_range(double theta_0, double theta_1, int divisions){
-  double *dists = new double[divisions + 1];
+double dists[divisions + 1];
+void scan_range(double theta_0, double theta_1, int divisions){
   double delta = (theta_1 - theta_0) / divisions;
   for (int i = 0; i <= divisions; i++){
     double angle = theta_0 + i * delta; // 2 * theta_1 since divisions is 2 * on second pass
     head.write(angle);
     if (i == 0) delay(300); // wait to get there
-    else delay(30);
+    else delay(20);
     double dist = watch();
+    if (dist < 0) dist = 0; // Filters out invalid values
     Serial.println(dist);
-    delay(30);
+    delay(20);
     dists[i] = dist;
     //Serial.println(angle);
   }
-  return dists;
+}
+
+double getMin(double* array, int size) {
+  double min = array[0];
+  for (int i = 1; i < size; i++) {
+    if (array[i] < min) min = array[i];
+  }
+  return min;
 }
 
 void setup() {
@@ -120,15 +147,17 @@ void setup() {
   
   head.attach(SERVO_PIN);
   head.write(90);
-  delay(2000);
+  delay(200);
   
   Serial.begin(9600);
   
   stop();
+  delay(200);
+  goForward();
 }
 
 void loop() {
-  double * dists = scan_range(0, 180, divisions);
+  scan_range(0, 180, divisions);
   double length = divisions * 2 + 1;
   double left_sum = 0;
   double right_sum = 0;
@@ -146,27 +175,55 @@ void loop() {
       left_sum += dists[i];
     }
   }
-  double left_average = left_sum / (divisions / 2);
   Serial.println("]");
-  if (straight >= 10 && left_average < 15){
-    forward();
-  }else if (left_average >= 15){
-    turnLeft();
-    Serial.print("Turning left");
-  }else{
-    turnRight();
-    Serial.print("Turning right");
+
+  // Find minimum value in the center
+  double * center_array = new double(divisions / 3);
+  Serial.print("Center array: [");
+  for (int i = 0; i <= divisions / 3; i++){
+    center_array[i] = dists[i + divisions / 3 + 1];
+    Serial.print(center_array[i]);
+    Serial.print(", ");
   }
-  //delay(200);
-  //stop();
-  /*
-  forward();
-  delay(1000);
-  turnLeft();
-  delay(1000);
-  turnRight();
-  delay(1000);
-  reverse();
-  delay(1000);
-  */
+  Serial.println("]");
+  double center_min = getMin(center_array, divisions / 3 + 1);
+  Serial.print("Center minimum: ");
+  Serial.println(center_min);
+
+  // Find minimum value on the left side
+  double * left_array = new double(divisions / 3);
+  Serial.print("Left array: [");
+  for (int i = 0; i <= divisions / 3; i++){
+    left_array[i] = dists[i + divisions *2/3 + 1];
+    Serial.print(left_array[i]);
+    Serial.print(", ");
+  }
+  Serial.println("]");
+  double left_min = getMin(left_array, divisions / 3 + 1);
+  Serial.print("Left minimum: ");
+  Serial.println(left_min);
+
+  // Logic to hug the left wall
+  Serial.println(state);
+  if (state == forward){
+    if (center_min >= 10 && left_min < 12 && left_min > 6){
+      goForward();
+    }else if (left_min >= 12){
+      turnLeft();
+    }else{
+      turnRight();
+    }
+  }
+  else if (state == right){
+    if (center_min < 10){
+      turnRight();
+    }
+    else{ goForward();}
+  }
+  else if (state == left){
+    if (left_min >= 12){
+      turnLeft();
+    }
+    else{ goForward();}
+  }
 }
