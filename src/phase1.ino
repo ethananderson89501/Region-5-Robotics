52fd157e3142e7449857ca89209c1df5cc8440c2
ethananderson89501 <ethananderson89501@gmail.com>
2025-03-17 16:27:19 -0500

    3_17 | Accurate position tracking, stuck counter, moving back to start

--- src/phase1.ino
+++ src/phase1.ino
@@ -49,6 +49,9 @@ int corners_checked = 0;
 int checking_state = 0;
 int target_found = 0;
 int checking_counter = 0;
+int finding_state = 0;
+int stuck_counter = 0;
+int finished = 0;
 
 //Odometry
 volatile unsigned long last_debounce_timeR = 0;
@@ -65,9 +68,18 @@ int currentCounterL = 0;
 int currentCounterR = 0;
 //Empirically determined constants from testing
 //double inches_per_tick = 0.29; // Old for clean treads
-double inches_per_tick = .346; // Measured from dirty treads
-double radians_per_tick = 0.0216; // Measured from dirty treads
-double turning_radius = 3;
+//double radians_per_tick = 0.0199; //Clean treads
+//double inches_per_tick = .346; // Measured from dirty treads
+//double radians_per_tick = 0.0216; // Measured from dirty treads
+//double turning_radius = 3;
+//double inches_per_tick = 0.307; //Tested 3/15
+//double radians_per_tick = 0.0209; // Tested 3/15
+//double inches_per_tick = 0.0700; // 3/16 both sensors - right reading more
+//double radians_per_tick = 0.00786; // 3/16 both sensors - right reading more
+//double inches_per_tick = 0.129; // 3/16 right sensor only
+//double radians_per_tick = .0116; // 3/16 right sensor only
+double inches_per_tick = 0.139; // 3/16 both sensors reading the same
+double radians_per_tick = 0.0188; // 3/16 both sensors reading the same
 
 // Distance from start position in inches
 // X is in the forward direction from the robot's initial position/orientation
@@ -93,7 +105,7 @@ int nav_state = 0;
 bool checkShouldExitObstacleAboidance(double angleto, double orientation){
   head.write(angleto - orientation);
   delay(30);
-  if watch() < 5{
+  if (watch() < 5){
     return true;
   }else{
     return false;
@@ -112,7 +124,7 @@ void goForward(){
 }
 
 void stop(){
-  //state = stop;
+  state = stop;
   analogWrite(IN1, 0);
   analogWrite(IN2, 0);
   analogWrite(IN3, 0);
@@ -144,7 +156,7 @@ void turnLeft(){
 void turnRight(){
   state = right;
   duty_cycleA = 128;
-  duty_cycleB = 191;
+  duty_cycleB = 128;
   analogWrite(IN1, duty_cycleA);
   analogWrite(IN2, 0);
   analogWrite(IN3, duty_cycleB);
@@ -154,7 +166,10 @@ void turnRight(){
 
 double watch(){
   double echo_distance = sonar.ping_in();
-  Serial.print("Distance: "); Serial.println(echo_distance);
+  if (echo_distance == 0){
+    echo_distance = 100;
+    Serial.println("Failed reading");
+  }
   return echo_distance;
 }
 
@@ -166,7 +181,7 @@ void scan_range(double theta_0, double theta_1, int divisions){
     if (direction == 1) head.write(angle);
     else head.write(theta_1 - angle);
     last_time1 = micros();
-    while (micros() < last_time1 + 30000){} // delay 
+    while (micros() < last_time1 + 40000){} // delay 
     double dist = watch();
     if (dist < 0) dist = 0; // Filters out invalid values
     if (direction == 1) dists[i] = dist;
@@ -184,9 +199,9 @@ double getMin(double* array, int size) {
   return min;
 }
 
-double center_array[divisions / 3];
-double left_array[divisions / 3];
-double right_array[divisions / 3];
+double center_array[divisions / 3 + 1];
+double left_array[divisions / 3 + 1];
+double right_array[divisions / 3 + 1];
 double center_min;
 double right_min;
 double left_min;
@@ -209,6 +224,7 @@ void getMins(){
     right_array[i] = dists[i];
   }
   right_min = getMin(right_array, divisions / 3 + 1);
+
   Serial.print("Distances: ");
   for (int i = 0; i < 9; i++){
     Serial.print(dists[i]);
@@ -219,52 +235,53 @@ void getMins(){
 }
 
 void navigate(){  // Logic to hug the left wall
-    if (state == forward){
-      if (center_min >= 10 && left_min < 30 && left_min > 4 && right_min > 4){
-        goForward();
-      }else if (left_min >= 10 && right_min > 4){
-        turnLeft();
-      }
-      
-      else if (backingOut && center_min >= 7) goForward();
-      else if (backingOut && left_min < 10){
-        backingOut = 0;
-        goForward();
-      }
-      
-      else if (right_min >= 8 & left_min > 4){
-        turnRight();
-      }
-      else{
-        backingOut = 1;
-        goReverse();
-        Serial.println("Backing out");
-      }
+  if (state == stop){
+    findWall();
+    finding_state = 1;
   }
-  else if (state == right){
-    if (center_min < 7){
+  if (state == forward){
+    if (center_min >= 9 && left_min < 12 && left_min > 6 && right_min > 6){
+      goForward();
+    }else if (left_min >= 12 && right_min > 6){
+      turnLeft();
+    }
+    /*
+    else if (backingOut && center_min >= 7) goForward();
+    else if (backingOut && left_min < 10){
+      backingOut = 0;
+      goForward();
+    }
+    */
+    else if (right_min >= 9 & left_min > 6){
       turnRight();
     }
-    else if (backingOut && right_min >= 12 && center_min < 10) turnRight();
+    else{
+      backingOut = 1;
+      goReverse();
+      Serial.println("Backing out");
+    }
+  }
+  else if (state == right){
+    if (center_min < 9){
+      turnRight();
+    }
+    else if (backingOut && right_min >= 14 && center_min < 12) turnRight();
     else {
       goForward();
       backingOut = 0;
     }
   }
   else if (state == left){
-    if (left_min >= 30){
+    if (left_min >= 12){
       turnLeft();
     }
-    else if (center_min > 7) goForward();
-    else if (right_min > 7) turnRight();
-    else{
-      backingOut = 1;
-      goReverse();
-    }
+    //else if (left_min > 4) { goForward();}
+    //else if (right_min >= 7) turnRight();
+    else goForward();// goReverse();
   }
   
   else if (state == reverse){
-    if (right_min >= 8){
+    if (right_min >= 10){
       turnRight();
       backingOut = 1;
     }
@@ -277,37 +294,38 @@ double targetY;
 double target_angle;
 
 void findCorner(){
-  Serial.print("State: "); Serial.println(checking_state);
-
   // State 0: Find the target
   if (checking_state == 0){
     // Target = (5, 5) away from the heat gun
     if (corners_checked == 0){ // No corners checked: goal is directly forward from start point
-      targetX = 17;
-      targetY = 8*12 - 17; 
+      targetX = 8;
+      targetY = 6*12; 
     }
     else if (corners_checked == 1){ // One corner checked: goal is opposite from start point
-      targetX = 8*12 - 17;
-      targetY = 8*12 - 17;
+      targetX = 8 + 5*12;
+      targetY = 6*12;
     }
     else if (corners_checked == 2){ // Two corners checked: goal is directly right from start point
-      targetX = 8*12 - 17;
-      targetY = 17;
+      targetX = 8 + 5*12;
+      targetY = 1*12;
     }
     else{
       Serial.println("Error: all corners have been checked");
     }
-    target_angle = atan((targetY - posY) / (targetX - posX));
-
     checking_state = 1;
   }
 
+  Serial.print("State: "); Serial.println(checking_state);
+  target_angle = atan((targetY - posY) / (targetX - posX));
+  if (targetX - posX < 0) target_angle = target_angle * -1; // Flip sign if we're in quadrants 2 or 3
+  Serial.print("Target Angle: "); Serial.println(target_angle);
+
   // State 1: Get proper orientation
   if (checking_state == 1){
-    Serial.print("Target Angle: "); Serial.println(target_angle);
     Serial.print("Orientation: "); Serial.println(orientation);
     if (target_angle - orientation < 10.0 / 180.0 * pi && target_angle - orientation > -10.0 / 180.0 * pi){ // If we're within 10 degrees of the target angle, start going forward
       checking_state = 2;
+      stuck_counter = 0;
     }
     // Otherwise, turn left or right depending on which is closer
     else if (target_angle - orientation < 0) turnRight();
@@ -316,21 +334,34 @@ void findCorner(){
 
   // State 2: Traveling toward the goal
   if (checking_state == 2){
-    if (posX > targetX - 2 && posX < targetX + 2 && posY > targetY - 2 && posY < targetY + 2){ // If we're at the target within +/- 2 inches
+    if (posX > targetX - 3 && posX < targetX + 3 && posY > targetY - 3 && posY < targetY + 3){ // If we're at the target within +/- 2 inches
       checking_state = 4;
+      stuck_counter = 0;
       last_time_temp = micros();
     }
-    else if (center_min < 5){
+    else if (abs(target_angle - orientation) > pi/2) checking_state = 1;
+    else if (center_min > 7){
       goForward();
     }
-    else checking_state = 4;
+    else{
+      checking_state = 3; // Obstalce avoidance
+      stuck_counter = 0;
+    }
+    //else checking_state = 4; // Just stop if you see an obstacle
   }
 
   // State 3: Obstacle avoidance
+  
   if (checking_state == 3){
-    if(checking_counter >= 10){ // Try to avoid obstacles for five iterations through the loop, then try again
+    if (posX > targetX - 5 && posX < targetX + 5 && posY > targetY - 5 && posY < targetY + 5){ // If we're at the target within +/- 2 inches
+      checking_state = 4;
+      stuck_counter = 0;
+      last_time_temp = micros();
+    }
+    else if(checking_counter >= 5 || posX > targetX + 10 || posX < targetX - 10 || posY > targetY + 10 || posY < targetY - 10){ // Try to avoid obstacles for five iterations through the loop, then try again
       checking_counter = 0;
       checking_state = 1;
+      stuck_counter = 0;
     }
     else{
       checking_counter++;
@@ -341,9 +372,11 @@ void findCorner(){
   // State 4: Checking temperature
   if (checking_state == 4){
     stop();
+    Serial.print("Last time temp: "); Serial.println(last_time_temp);
     if (corners_checked == 2) target_found = 1;
     else{
       temp = analogRead(A0) / 1024.0 * 5 * 100; // temperature in degrees C
+      Serial.print("Temp: "); Serial.println(temp);
       if (temp > 35){ // Adjust temperature threshold as neccessary
         target_found = 1;
       }
@@ -353,8 +386,45 @@ void findCorner(){
         if (elapsed_time_temp > 3000000){ // Stay for three seconds to allow temperature sensor to adjust
           corners_checked++;
           corner = 0;
+          checking_state = 0;
         }
       }
+    }
+
+    stuck_counter++;
+    if (stuck_counter > 10){ // Adjust this value while testing
+      stuck_counter = 0;
+      checking_state = 4;
+    }
+  }
+
+}
+
+void findWall(){
+  Serial.println("Finding wall");
+
+  if (corners_checked == 0) target_angle = pi/2;
+  if (corners_checked == 1) target_angle = 0;
+  if (corners_checked == 2) target_angle = -pi/2;
+  Serial.print("Target angle: "); Serial.println(target_angle);
+
+  // State 1: target angle
+  if (finding_state == 1){
+    Serial.print("Orientation: "); Serial.println(orientation);
+    if (target_angle - orientation < 10.0 / 180.0 * pi && target_angle - orientation > -10.0 / 180.0 * pi){ // If we're within 10 degrees of the target angle, start going forward
+      finding_state = 2;
+    }
+    // Otherwise, turn left or right depending on which is closer
+    else if (target_angle - orientation < 0) turnRight();
+    else if (target_angle - orientation > 0) turnLeft();
+  }
+
+  // State 2: Moving forward
+  if (finding_state == 2){
+    goForward();
+    if (center_min < 12 || left_min < 12 || right_min < 12){
+      finding_state = 0;
+      Serial.println("Returning to normal");
     }
   }
 
@@ -362,7 +432,7 @@ void findCorner(){
 
 void countL() {
   current_time = micros();
-  if (current_time > last_debounce_timeL + 3000) {  // Only process on change
+  if (current_time > last_debounce_timeL + 10000) {  // Only process on change
     leftCounter++;
     last_debounce_timeL = current_time;
   }  
@@ -370,7 +440,7 @@ void countL() {
 
 void countR() {
   current_time = micros();
-  if (current_time > last_debounce_timeR + 3000) {  // Only process on change
+  if (current_time > last_debounce_timeR + 10000) {  // Only process on change
     rightCounter++;
     last_debounce_timeR = current_time;
   }  
@@ -387,25 +457,25 @@ void get_position(){
 
   //Calculate position based on what state we're in
   if (state == forward){
-    double distance_traveled = (currentCounterL + currentCounterR) / 2.0 * inches_per_tick;
+    double distance_traveled = (currentCounterL + currentCounterR) * inches_per_tick;
     posX = posX + cos(orientation) * distance_traveled;
     posY = posY + sin(orientation) * distance_traveled;
   }
   else if (state == reverse){
-    double distance_traveled = (currentCounterL + currentCounterR) / 2.0 * inches_per_tick;
+    double distance_traveled = (currentCounterL + currentCounterR) * inches_per_tick;
     posX = posX - cos(orientation) * distance_traveled;
     posY = posY - sin(orientation) * distance_traveled;
   }
   else if (state == left){
     double dori = (currentCounterL + currentCounterR) * radians_per_tick;
-    posX = posX + turning_radius * cos(dori + orientation)  - turning_radius * cos(orientation);
-    posY = posY + turning_radius * sin(dori + orientation) - turning_radius * sin(orientation);;
+    //posX = posX + turning_radius * cos(dori + orientation)  - turning_radius * cos(orientation);
+    //posY = posY + turning_radius * sin(dori + orientation) - turning_radius * sin(orientation);;
     orientation += dori;
   }
   else if (state == right){
     double dori = -1 * (currentCounterL + currentCounterR) * radians_per_tick;
-    posX = posX + turning_radius * cos(dori + orientation)  - turning_radius * cos(orientation);
-    posY = posY + turning_radius * sin(dori + orientation) - turning_radius * sin(orientation);;
+    //posX = posX + turning_radius * cos(dori + orientation)  - turning_radius * cos(orientation);
+    //posY = posY + turning_radius * sin(dori + orientation) - turning_radius * sin(orientation);;
     orientation += dori;
   }
   
@@ -414,7 +484,7 @@ void get_position(){
   lastTotalCounterL = totalCounterL;
   lastTotalCounterR = totalCounterR;
 
-  Serial.print("Position: "); Serial.print(posX); Serial.print(", "); Serial.println(posY);
+  //Serial.print("Position: "); Serial.print(posX); Serial.print(", "); Serial.println(posY);
 } 
 
 void setup() {
@@ -463,76 +533,51 @@ void setup() {
 }
 
 void loop() {
-  
-  scan_range(0, 180, divisions); // This is where the most time is spent. Gathering obstacle data.
+  if (!target_found){
+    scan_range(0, 180, divisions); // This is where the most time is spent. Gathering obstacle data.
 
-  get_position(); // Calculate our current position.
+    get_position(); // Calculate our current position.
 
-  // Check to see if we've found the next corner
-  if (!corner){
-    if (corners_checked == 0){ // Target: 1ft, 7ft
-      if (posY > 6*12) corner = 1; //If we've gone forward 6 ft, find the corner
+    temp = analogRead(A0) / 1024.0 * 5 * 100; // temperature in degrees C
+    if (temp > 35){
+      stop();
+      target_found = 1;
+      Serial.print("Temp: "); Serial.println(temp);
     }
-    if (corners_checked == 1){ //Target: 7ft, 7ft
-      if (posX > 6*12) corner = 1; //If we've traveled to the first corner, and then right 6 ft, find the corner
+
+    else{
+      // Check to see if we've found the next corner
+      if (!corner){
+        if (corners_checked == 0){ // Target: 1ft, 7ft
+          if (posY > 5*12) corner = 1; //If we've gone forward 6 ft, find the corner
+        }
+        if (corners_checked == 1){ //Target: 7ft, 7ft
+          if (posX > 5*12) corner = 1; //If we've traveled to the first corner, and then right 6 ft, find the corner
+        }
+        if (corners_checked == 2){ //Target: 7ft, 1ft
+          if (posY < 2*12) corner = 1; //If we're within 1 ft of our original y position, find the corner
+        }
+      }
+      getMins();
+      if (finding_state) findWall();
+      else if (!corner) navigate(); //Hug the left wall
+      else findCorner();
     }
-    if (corners_checked == 2){ //Target: 7ft, 1ft
-      if (posY < 1*12) corner = 1; //If we're within 1 ft of our original y position, find the corner
-    }
-  }
 
-  getMins();
-  if (!corner) navigate(); //Hug the left wall
-  else findCorner();
-
-  ////////////// Navigation testing logic //////////////////////////
-  /*
-  get_position();
-  current_time = micros();
-  elapsed_time1 = current_time - last_time1;
-  elapsed_time2 = current_time - last_time2;
-
-  if (elapsed_time1 > 500000 && !no_print){
-    get_position();
-    //Serial.print("Total left coutner: "); Serial.println(totalCounterL);
-    //Serial.print("Total right counter:"); Serial.println(totalCounterR);
-    Serial.print("Left counter: "); Serial.println(currentCounterL);
-    Serial.print("Right counter: "); Serial.println(currentCounterR);
     Serial.print("Position: "); Serial.print(posX); Serial.print(", "); Serial.println(posY);
     Serial.print("Orientation: "); Serial.println(orientation * 180 / pi);
-    last_time1 = current_time;
-    //no_print = 1;
-  }
-
-  if (current_time > 3000000 && !no_print){
-    stop();
-    get_position();
     Serial.print("Total left coutner: "); Serial.println(totalCounterL);
     Serial.print("Total right counter:"); Serial.println(totalCounterR);
-    Serial.print("Position: "); Serial.print(posX); Serial.print(", "); Serial.println(posY);
-    Serial.print("Orientation: "); Serial.println(orientation * 180 / pi);
-    no_print = 1;
   }
-  
 
-  if (elapsed_time2 > 10000){
-    if (nav_state == 0){
-      goForward();
-      if (posY > 6*12) nav_state = 3;
-    }
-    if (nav_state == 1){
-      turnRight();
-      if (orientation < 0) nav_state = 2;
-    }
-    if (nav_state == 2){
-      goForward();
-      if (posX > 6*12){
-        nav_state = 3;
-      }
-    }
-    if (nav_state == 3){
-      findCorner();
+  else if (!finished){
+    scan_range(0, 180, divisions); // This is where the most time is spent. Gathering obstacle data.
+    get_position(); // Calculate our current position.
+    navigate();
+    if (posX < 2*12 && posY < 2*12){ // Might adjust this later. Front wheel needs to be within a 1.5ft radius of the starting region
+      stop();
+      noInterrupts();
+      finished = 1;
     }
   }
-  */
 }
